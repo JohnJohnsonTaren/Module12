624f1c628368686d02fd00af11524636a7749a21
JohnJohnsonTaren <tytar.den@gmail.com>
2025-04-04 19:35:47 +0300

    https://github.com/JohnJohnsonTaren/Module12.git

--- src/main/java/Task2/Main.java
+++ src/main/java/Task2/Main.java
@@ -4,110 +4,167 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
+    private static final int n = 15;
+    private static int current = 1;
+    private static final Object lock = new Object();
+    private static final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
     public static void main(String[] args) {
-        BlockingQueue<String> queue = new LinkedBlockingQueue<>();
-
-        Thread a = new Thread(new Fizz(queue));
+        Thread a = new Thread(new Fizz());
         a.start();
 
-        Thread b = new Thread(new Buzz(queue));
+        Thread b = new Thread(new Buzz());
         b.start();
 
-        Thread c = new Thread(new FizzBuzz(queue));
+        Thread c = new Thread(new FizzBuzz());
         c.start();
 
-        Thread d = new Thread(new Number(queue));
+        Thread d = new Thread(new Number());
         d.start();
+
+        try {
+            a.join();
+            b.join();
+            c.join();
+            d.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     static class Number implements Runnable {
-        private final BlockingQueue<String> queue;
-
-        public Number(BlockingQueue<String> queue) {
-            this.queue = queue;
-        }
-
         @Override
         public void run() {
-            for (int i = 1; i <= 15; i++) {
-                if (i % 3 != 0 && i % 5 != 0) {
-                    System.out.println(i);
-                } else {
-                    try {
-                        System.out.println(queue.take());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            while (current <= n) {
+                synchronized (lock) {
+                    while (current <= n && (current % 3 == 0 || current % 5 == 0)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
                     }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (current > n) {
+                        lock.notifyAll();
+                        break;
+                    }
+                    System.out.println(current);
+                    current++;
+                    lock.notifyAll();
                 }
             }
         }
     }
 
     static class Fizz implements Runnable {
-        private final BlockingQueue<String> queue;
-
-        public Fizz(BlockingQueue<String> queue) {
-            this.queue = queue;
-        }
-
         @Override
         public void run() {
-            for (int i = 1; i <= 15; i++) {
-                if (i % 3 == 0 && i % 5 != 0) {
-                    queue.add(i + " Fizz");
-                }
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            while (current <= n) {
+                synchronized (lock) {
+                    while (current <= n && !(current % 3 == 0 && current % 5 != 0)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (current > n) {
+                        lock.notifyAll();
+                        break;
+                    }
+                    try {
+                        queue.put("Fizz");
+                        System.out.println(current + " " + queue.take());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    current++;
+                    lock.notifyAll();
                 }
             }
         }
     }
 
     static class Buzz implements Runnable {
-        private final BlockingQueue<String> queue;
-
-        public Buzz(BlockingQueue<String> queue) {
-            this.queue = queue;
-        }
-
         @Override
         public void run() {
-            for (int i = 1; i <= 15; i++) {
-                if (i % 5 == 0 && i % 3 != 0) {
-                    queue.add(i + " Buzz");
-                }
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            while (current <= n) {
+                synchronized (lock) {
+                    while (current <= n && !(current % 5 == 0 && current % 3 != 0)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (current > n) {
+                        lock.notifyAll();
+                        break;
+                    }
+                    try {
+                        queue.put("Buzz");
+                        System.out.println(current + " " + queue.take());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    current++;
+                    lock.notifyAll();
                 }
             }
         }
     }
 
     static class FizzBuzz implements Runnable {
-        private final BlockingQueue<String> queue;
-
-        public FizzBuzz(BlockingQueue<String> queue) {
-            this.queue = queue;
-        }
-
         @Override
         public void run() {
-            for (int i = 1; i <= 15; i++) {
-                if (i % 3 == 0 && i % 5 == 0) {
-                    queue.add(i + " FizzBuzz");
-                }
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            while (current <= n) {
+                synchronized (lock) {
+                    while (current <= n && !(current % 15 == 0)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (current > n) {
+                        lock.notifyAll();
+                        break;
+                    }
+                    try {
+                        queue.put("FizzBuzz");
+                        System.out.println(current + " " + queue.take());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    current++;
+                    lock.notifyAll();
                 }
             }
         }
